7b093ed1efaa00cf9e74450707244c6c68d6fd0e
MohDAdamSTTN <sithuthetnaing@hexcode.tech>
2026-05-25 22:39:51 +0700

    fix: seeding and login logs

---
+++ internal/audit/audit.go
@@ -0,0 +1,31 @@
+package audit
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/MohdAdamSiThuThetNaing/technical-test-fluxion/internal/queue"
+)
+
+func PublishAuditLog(
+	c *gin.Context,
+	event string,
+	userID string,
+	email string,
+	name string,
+	reason string,
+) {
+
+	queue.Publish(gin.H{
+		"event":        event,
+		"user_id":      userID,
+		"user_email":   email,
+		"updated_name": name,
+		"reason":       reason,
+		"ip_address":   c.ClientIP(),
+		"user_agent":   c.Request.UserAgent(),
+		"action_by":    email,
+		"created_at":   time.Now(),
+	})
+}
--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 
+	"github.com/MohdAdamSiThuThetNaing/technical-test-fluxion/internal/audit"
 	"github.com/MohdAdamSiThuThetNaing/technical-test-fluxion/internal/db"
 	"github.com/MohdAdamSiThuThetNaing/technical-test-fluxion/internal/users"
 )
@@ -26,7 +27,17 @@ func Login(c *gin.Context) {
 		Where("email = ?", email).
 		First(&user).Error
 
+	// User not found
 	if err != nil {
+		audit.PublishAuditLog(
+			c,
+			"AUTH_LOGIN_FAILED",
+			"",
+			email,
+			"",
+			"USER_NOT_FOUND",
+		)
+
 		c.HTML(
 			http.StatusUnauthorized,
 			"login.html",
@@ -34,15 +45,26 @@ func Login(c *gin.Context) {
 				"Error": "Invalid credentials",
 			},
 		)
+
 		return
 	}
 
+	// Invalid password
 	err = bcrypt.CompareHashAndPassword(
 		[]byte(user.Password),
 		[]byte(password),
 	)
 
 	if err != nil {
+		audit.PublishAuditLog(
+			c,
+			"AUTH_LOGIN_FAILED",
+			user.ID.String(),
+			user.Email,
+			user.Name,
+			"INVALID_PASSWORD",
+		)
+
 		c.HTML(
 			http.StatusUnauthorized,
 			"login.html",
@@ -50,16 +72,25 @@ func Login(c *gin.Context) {
 				"Error": "Invalid credentials",
 			},
 		)
+
 		return
 	}
 
 	session := sessions.Default(c)
+
 	session.Set("authenticated", true)
 	session.Set("user_id", user.ID.String())
 	session.Set("user_email", user.Email)
 	session.Set("user_name", user.Name)
 	session.Save()
-
+	audit.PublishAuditLog(
+		c,
+		"AUTH_LOGIN_SUCCESS",
+		user.ID.String(),
+		user.Email,
+		user.Name,
+		"",
+	)
 	c.Redirect(http.StatusFound, "/")
 }
 
